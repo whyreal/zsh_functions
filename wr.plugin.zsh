1c5d15e8894da832337759fdb2a32210f42183c7
y_real <y_real@qq.com>
2020-04-09 17:21:56 +0800

    update

--- wr.plugin.zsh
+++ wr.plugin.zsh
@@ -1,1 +1,182 @@
+# 环境变量 {{{1
+# history
+export HISTSIZE=5000
+export HISTFILESIZE=50000
+# WORDCHARS
+#export WORDCHARS='-*?_[]~=&;!#$%^(){}'
+# editor
+export EDITOR='vim'
+# pager
+#export PAGER='bat'
+# use vim as man pager
+export MANPAGER="vim -c 'MANPAGER' -c 'set fdm=indent ts=7 sw=7' -"
 
+export LC_ALL="en_US.UTF-8"
+#export LC_ALL="zh_CN.GB2312"
+if [[ $LC_CTYPE = "zh_CN.eucCN" ]]; then
+    export NLS_LANG=AMERICAN_AMERICA.ZHS16GBK
+fi
+
+# golang
+export PATH=$PATH:$HOME/go/bin/
+export GOPATH=/Users/Real/go
+export GO111MODULE=on # Enable the go modules feature
+export GOPROXY=https://goproxy.cn #Set the GOPROXY environment variable
+
+# java
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk8u212-b03/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH 
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar 
+export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-1.0.0-rc14/Contents/Home
+
+export PATH=$PATH:/Users/Real/Documents/Note/scripts/
+export PATH=$PATH:/usr/local/sbin
+export PATH=$PATH:/usr/local/opt/mysql-client/bin
+export PATH=$PATH:$HOME/.yarn/bin
+export PATH=$PATH:$HOME/.config/yarn/global/node_modules/.bin
+export PATH=$PATH:/Applications/instantclient_18_1
+# }}}
+# Alias {{{1
+alias z='_zlua -I'
+alias pwgen='pwgen -r0oOiIlL'
+alias grep='grep --color'
+alias tr='tmux -u attach'
+alias ....='cd ../../..'
+alias ...='cd ../..'
+alias ..='cd ..'
+#alias pd='pushd'
+#alias d='dirs -v'
+alias s='sshrc'
+alias r='open -R'
+alias vim='nvim'
+# }}}
+# Functions {{{1
+
+favorite.docs(){
+    abs_path=`greadlink -f $1`
+    cd ~/Documents/vim-workspace/docs/ && ln -s $abs_path ./$2
+}
+
+o(){ # {{{2
+
+    file=$1
+    mime_type="${$(file --mime-type $file | awk '{print $2}')%/*}"
+
+    if [[ $mime_type = 'text' ]]; then
+        vim $file
+    else
+        open $file
+    fi
+}
+# }}}
+c(){ # {{{2
+    cd $1
+    ls
+}
+# }}}
+vs(){ # {{{2
+    mvim -n \
+        -c "let w:remote_host=\"$1\"" \
+        -c "let &titlestring=w:remote_host" \
+        -c "Edit /root/" \
+        -c "new" -c "ServerUpdateInfo"
+    sshrc $1
+}
+# }}}
+lf(){ #{{{2
+    tmp="$(mktemp)"
+    ~/go/bin/lf --last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        if [ -d "$dir" ]; then
+            if [ "$dir" != "$(pwd)" ]; then
+                cd "$dir"
+            fi
+        fi
+    fi
+}
+# }}}
+vpn.route.add.zkzy() { # {{{2
+    sudo route -n add 192.168.80/24 -interface ppp0
+}
+# }}}
+vpn.route.del.zkzy() { # {{{2
+    sudo route -n delete 192.168.80/24 -interface ppp0
+}
+# }}}
+safari.agent.ipad() { # {{{2
+    defaults write com.apple.Safari CustomUserAgent "'Mozilla/5.0 (iPad; CPU OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B176 Safari/7534.48.3'"
+}
+# }}}
+safari.agent.default() { # {{{2
+    defaults delete com.apple.Safari CustomUserAgent
+}
+# }}}
+proxy.ss.active(){ # {{{2
+    export http_proxy="http://127.0.0.1:1080"
+    export https_proxy="http://127.0.0.1:1080"
+}
+# }}}
+proxy.ss.deactive(){ # {{{2
+    export http_proxy=""
+    export https_proxy=""
+}
+# }}}
+update.all() { # {{{2
+
+    echo "-------- update antigen--------"
+    antigen update
+
+    echo "-------- update tmux--------"
+    /Users/Real/.tmux/plugins/tpm/bin/update_plugins all
+
+    echo "-------- update homebrew--------"
+    brew upgrade
+    brew cleanup
+
+    echo "-------- update Coc--------"
+    vim -c 'PlugUpdate | CocUpdateSync | qall'
+}
+# }}}
+oracle.env.gb2312(){ # {{{2
+	echo export NLS_LANG=AMERICAN_AMERICA.ZHS16GBK
+}
+# }}}
+color.print(){    #{{{2
+    for C in {40..47}; do
+        echo -en "\e[${C}m$C "
+    done
+
+    # high intensity colors
+    for C in {100..107}; do
+        echo -en "\e[${C}m$C "
+    done
+
+    # 256 colors https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit
+    for C in {16..255}; do
+        echo -en "\e[48;5;${C}m$C "
+    done
+
+    echo -e "\e(B\e[m"
+}
+#}}}
+color.print.tput(){ #{{{2
+    for C in {0..255}; do
+        tput setab $C
+        echo -n "$C "
+    done
+    tput sgr0
+}
+alias telnet='nc -vz -w 1'
+#}}}
+ssh.excute_local_script() { # {{{2
+    h=$1
+    s=$2
+    cat $2 | ssh -T $1
+}
+# }}}
+# }}}
+
+# vim: fdm=marker sw=4 ts=4 et ft=zsh
